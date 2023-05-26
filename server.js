6c5f28eb22c02186592ed26f5dbb55b30353799b
Rabiul Hasan <rhrobi2015@gmail.com>
2023-05-26 23:05:19 +0600

    server create

---
+++ server.js
@@ -0,0 +1,53 @@
+import Express  from "express";
+import dotenv from "dotenv"
+const app = Express()
+dotenv.config()
+app.use(Express.json())
+
+const port = process.env.PORT || 8000
+
+app.get('/:postition',(req, res)=>{
+      const test = []
+
+      test.push(
+            {
+                  'id': 122344,
+                  'name': 'testStudent',
+                  'dept':'cse',
+                  'semeter': 6
+            }
+      )
+
+      test.push(
+            {
+                  'id': 1223465,
+                  'name': 'testStudent1',
+                  'dept':'BBA',
+                  'semeter': 7
+            }
+      )
+
+      test.push(
+            {
+                  'id': 12234545,
+                  'name': 'testStudent2',
+                  'dept':'textile',
+                  'semeter': 7
+            }
+      )
+
+      test.push(
+            {
+                  'id': 122345,
+                  'name': 'testStudent4',
+                  'dept':'english',
+                  'semeter': 2
+            }
+      )
+
+      res.status(200).json(test[req.params.postition])
+})
+
+app.listen(port, ()=>{
+      console.log(`my application is listing on ${port}`)
+})
